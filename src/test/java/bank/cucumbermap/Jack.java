abeb8192cca323887af9f7f9544e7950826fdecc
ganesh <ganesh.nilsoft@gmail.com>
2021-02-25 19:13:25 +0530

    by jack

---
+++ src/test/java/bank/cucumbermap/Jack.java
@@ -0,0 +1,20 @@
+package bank.cucumbermap;
+
+public class Jack 
+{
+	
+	
+	
+	
+	
+        public static void main(String[] args) 
+        {
+        	
+        	System.out.println("Jack works");
+			
+		}	
+	
+	
+
+
+}
